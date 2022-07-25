b97f408498369c8281bd0540a9d2406635fdffc8
Tynka <marcia03835@gmail.com>
2022-07-25 23:10:59 +0200

    Added method is_full? and is_winner? to class Board

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,8 +1,8 @@
-class Board  #need some changes and new method
-    # WINNING_COMBINATIONS = [
-    #     [0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6],
-    #     [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]
-    #   ]
+class Board  
+     WINNING_COMBINATIONS = [
+         [0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6],
+         [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]
+      ]
     def initialize
         @board = [1,2,3,4,5,6,7,8,9]
     end
@@ -18,7 +18,16 @@ class Board  #need some changes and new method
     def change_symbol(symbol, number)
         @board[number - 1] = symbol
     end
-    def is_winner?(player_moves)
+    def is_full?()
+        @board.all? do |cell|
+            cell.to_s.match?(/[[:alpha:]]/)
+        end
+    end
+    def is_winner?(symbol)
+        WINNING_COMBINATIONS.each do |combination| 
+            if @board[combination[0]].to_s == symbol && @board[combination[1]].to_s == symbol && @board[combination[2]].to_s == symbol
+                return true
+            end                
         end
     end
 end
@@ -26,10 +35,18 @@ end
 class Player
     attr_reader :name, :symbol
     $players = {}
-    def initialize(name, symbol) #maybe need changes
+    def initialize(name, symbol) 
         @name = name
         @symbol = symbol
         $players[@name] = @symbol
     end
-end
+end 
+
+game_board = Board.new
+player_one = Player.new("PlayerOne", "X")
+player_two = Player.new("PlayerTwo", "O")
+
+game_board.draw
+
+puts game_board.is_full?
 
