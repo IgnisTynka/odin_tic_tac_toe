c6690c749cad78a2a28cae0d74d475880ab1ed51
Tynka <marcia03835@gmail.com>
2022-07-26 21:11:58 +0200

    Final version

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -5,7 +5,7 @@ class Board
          [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]
       ]
     def initialize
-        @board = [1,2,3,4,5,6,7,8,9]
+        @board = ["1","2","3","4","5","6","7","8","9"]
         @player_one = Player.new("PlayerOne", "X")
         @player_two = Player.new("PlayerTwo", "O")
     end
@@ -24,25 +24,25 @@ class Board
         else 
             if $player_move_id.even?
                 @board[number - 1] = @player_one.symbol
-                $player_moeve_id = 1
+                $player_move_id = 1
             else
                 @board[number - 1] = @player_two.symbol
-                $player_moeve_id = 0
+                $player_move_id = 0
             end
-        end
-            
+        end    
     end
     def is_full?()
         @board.all? do |cell|
             cell.to_s.match?(/[[:alpha:]]/)
         end
     end
-    def is_winner?(symbol)
+    def is_winner?()
         WINNING_COMBINATIONS.each do |combination| 
-            if @board[combination[0]].to_s == symbol && @board[combination[1]].to_s == symbol && @board[combination[2]].to_s == symbol
+            if @board[combination[0]] == @board[combination[1]] && @board[combination[1]] == @board[combination[2]]
                 return true
             end                
         end
+        return false
     end
 end
 
@@ -58,11 +58,20 @@ end
 
 game_board = Board.new
 
+game_board.draw
 
-until game_board.is_full?
+until game_board.is_full? || game_board.is_winner?()
     number = gets.chomp.to_i
     game_board.change_symbol(number)
     game_board.draw
 end
 
-
+if(game_board.is_winner?)
+    if($player_move_id)
+        puts "Player One win!"
+    else
+        puts "Player Two win!"
+    end
+else 
+    puts "Draw!"
+end
